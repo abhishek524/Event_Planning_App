9fbb9d74b1cc0e032b8033495ddb582fbf7c641f
abhishek524 <mivicky524@gmail.com>
2024-03-10 23:38:07 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,12 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.get("/", (req,res) => {
+  res.json({
+    success: true,
+    message: "hello backend"
+  })
+})
 app.use("/api/v1/message", messageRouter);
 
 dbConnection();
